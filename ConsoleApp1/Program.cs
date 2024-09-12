2a399d69d674951f83a9b1237fdf71ca23b75dc3
Shree2303 <shreeprabhakaran2303@gmail.com>
2024-09-12 16:19:53 +0530

    Console Writeline is duplicated.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! This is Shruthi Shree :) ");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! This is Shruthi Shree :) ");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! This is Shruthi Shree :) ");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! This is Shruthi Shree :) ");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! This is Shruthi Shree :) ");
+
+
+
+
+
+
