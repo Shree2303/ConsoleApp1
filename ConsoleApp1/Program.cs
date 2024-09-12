ec3caa7d3f695d60db4f0979ad31e48f20c6e8ac
Shree2303 <shreeprabhakaran2303@gmail.com>
2024-09-12 16:35:37 +0530

    First commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,16 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! This is Shruthi Shree :) ");
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! This is Shruthi Shree :) ");
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! This is Shruthi Shree :) ");
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! This is Shruthi Shree :) ");
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! This is Shruthi Shree :) ");
+﻿myname();
+
+    void myname()
+{
+    Console.WriteLine("Shruthi");
+}
 
 
+myCompany();
 
+void myCompany()
+{
+    Console.WriteLine("Williams Le");
+}
 
+myCompany();
 
+void myHostel()
+{
+    Console.WriteLine("Mahalaksmi PG ");
+}
 
