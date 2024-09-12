5c95ac2f8d8477e0a38dcbe817d75d09d8967c39
Shree2303 <shreeprabhakaran2303@gmail.com>
2024-09-12 16:25:23 +0530

    Revert "Keeping one Console"
    This reverts commit 0a9aee51d6011e1dad479bd7c67376f804aa5ac9.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! This is Shruthi Shree :) ");
-
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! This is Shruthi Shree :) ");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! This is Shruthi Shree :) ");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! This is Shruthi Shree :) ");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! This is Shruthi Shree :) ");
 
 
 
