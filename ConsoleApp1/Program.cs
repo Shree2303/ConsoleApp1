0a9aee51d6011e1dad479bd7c67376f804aa5ac9
Shree2303 <shreeprabhakaran2303@gmail.com>
2024-09-12 16:21:00 +0530

    Keeping one Console

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,13 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! This is Shruthi Shree :) ");
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! This is Shruthi Shree :) ");
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! This is Shruthi Shree :) ");
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! This is Shruthi Shree :) ");
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! This is Shruthi Shree :) ");
+
 
 
 
